1b910234ddf7e21edcba6a3df37da7c8f19f7ce0
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-07-15 12:43:13 -0400

    Add UI to game

--- script.js
+++ script.js
@@ -44,32 +44,78 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+function updateUI(
+  playerSelection,
+  computerSelection,
+  result,
+  playerScore,
+  computerScore,
+  gameResult
+) {
+  const h2PlayerSelection = document.querySelector(".player-selection");
+  const h2ComputerSelection = document.querySelector(".computer-selection");
+  const h2Result = document.querySelector(".result");
+  const divScoreContainer = document.querySelector(".score-container");
+  const h2PlayerScore = document.querySelector(".player-score");
+  const h2ComputerScore = document.querySelector(".computer-score");
+  const h2GameResult = document.querySelector(".game-result");
+
+  h2PlayerSelection.innerHTML = `Player Selection: ${playerSelection}`;
+  h2ComputerSelection.innerHTML = `Computer Selection: ${computerSelection}`;
+  h2Result.innerHTML = result;
+  divScoreContainer.style.border = "5px solid black";
+  divScoreContainer.style.padding = "0 20px";
+  h2PlayerScore.innerHTML = `Player Score: ${playerScore}`;
+  h2ComputerScore.innerHTML = `Player Computer: ${computerScore}`;
+  h2GameResult.innerHTML = gameResult;
+}
+
 function game() {
   let playerScore = 0;
   let computerScore = 0;
+  const options = document.querySelectorAll(".option");
+  options.forEach((option) =>
+    option.addEventListener("click", () => {
+      // play round
+      const playerSelection = option.innerHTML;
+      const computerSelection = computerPlay();
+      const result = playRound(playerSelection, computerSelection);
+      if (result.includes("You Win!")) {
+        playerScore++;
+      } else if (result.includes("You Lose!")) {
+        computerScore++;
+      }
 
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Rock, Paper, or Scissors?");
-    let computerSelection = computerPlay();
-    result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    if (result.includes("You Win!")) {
-      playerScore++;
-    } else if (result.includes("You Lose!")) {
-      computerScore++;
-    }
-    console.log(`Score:
-    Player Score: ${playerScore}
-    Computer Score: ${computerScore}`);
-  }
+      // game result
+      let gameResult;
+      if (playerScore > computerScore) {
+        gameResult = "Player Wins!";
+      } else if (playerScore < computerScore) {
+        gameResult = "Computer Wins!";
+      } else {
+        gameResult = "Game is a Tie!";
+      }
 
-  if (playerScore > computerScore) {
-    console.log("Player Wins!");
-  } else if (playerScore < computerScore) {
-    console.log("Computer Wins!");
-  } else {
-    console.log("Game is a Tie!");
-  }
+      updateUI(
+        playerSelection,
+        computerSelection,
+        result,
+        playerScore,
+        computerScore,
+        gameResult
+      );
+    })
+  );
 }
+
+// event listeners
+// const option = document.querySelectorAll(".option");
+// option.forEach((selection) =>
+//   selection.addEventListener("click", () => {
+//     console.log(selection.innerHTML.toLowerCase());
+//     const h2PlayerSelection = document.querySelector(".h2-selection");
+//     h2PlayerSelection.innerHTML = `Player Selection: ${selection.innerHTML}`;
+//   })
+// );
 
 game();
